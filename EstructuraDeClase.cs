c9c6b9cc681da29dc0c6debfeae31d7876632942
Syo0131 <164595419+Syo0131@users.noreply.github.com>
2024-05-26 13:04:26 -0400

    Create EstructuraDeClase.cs

---
+++ EstructuraDeClase.cs
@@ -0,0 +1,8 @@
+public class MiembrosDeLaComunidad {}
+public class Empleado : MiembrosDeLaComunidad {}
+public class Estudiante : MiembrosDeLaComunidad {}
+public class ExAlumno : MiembrosDeLaComunidad {}
+public class Docente : Empleado {}
+public class Administrativo : Empleado {}
+public class Administrador : Docente {}
+public class Maestro : Docente {}
